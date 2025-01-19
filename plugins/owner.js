6a45a77a6e9b1122e2b8022356ee9db226458de2
Dinuwa-md <dinuwamdnew2007@gmail.com>
2025-01-19 11:54:08 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,9 +11,9 @@ cmd({
 async (conn, mek, m, { from }) => {
     try {
         // Owner's contact info
-        const ownerNumber = '94727680749'; // Replace this with the actual owner number
-        const ownerName = '➺ᴍʀ ᴅɪɴᴇꜱʜ࿐'; // Replace this with the owner's name
-        const organization = 'QUEEN SADU TEAM'; // Optional: replace with the owner's organization
+        const ownerNumber = '94727163302'; // Replace this with the actual owner number
+        const ownerName = '➺ᴍʀ ᴅɪɴᴜᴡᴀ࿐'; // Replace this with the owner's name
+        const organization = 'DINUWA MD TEAM'; // Optional: replace with the owner's organization
 
         // Create a vCard (contact card) for the owner
         const vcard = 'BEGIN:VCARD\n' +
